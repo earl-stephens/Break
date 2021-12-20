85e2e2da18b7761185666bb8af42cf893638fe6f
earl-stephens <stephensearl@hotmail.com>
2021-12-19 20:07:23 -0700

    Exploring uses of break

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,20 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int count = 0;
+		
+		//the break will break out of the infinite loop
+		while(true) {
+			System.out.println("Hello. " + count);
+			
+			if(count == 5) {
+				break;
+			}
+			System.out.println("How are you?");
+			count++;
+		}
+		System.out.println("Program finished.");
 
 	}
 
